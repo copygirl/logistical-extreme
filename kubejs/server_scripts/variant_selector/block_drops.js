d13d6c530dbf5eacad9822af7e9540855a22f49c
copygirl <copygirl@mcft.net>
2025-03-31 09:16:13 +0200

    Split variant selector script into multiple files

--- kubejs/server_scripts/variant_selector/block_drops.js
+++ kubejs/server_scripts/variant_selector/block_drops.js
@@ -1,39 +1,6 @@
-// See also: config/quark-common.toml
-// experimental.variant_selector.variants
-global.yeet([ "viewers" ],
-// FIXME: Once we have recipes figured out, add "recipes" back in.
-// global.yeet([ "viewers", "recipes" ],
-
-    // ==== Common ====
-    /_slab$/,                  // Includes vertical slab.
-    /_stairs$/,
-    /_fence$/,
-    /_gate$/,
-    /:(?!paper).*_wall$/,      // Don't include Quark paper wall.
-    /_carpet$/,                // Includes leaf carpet.
-    /_pane$/,
-    /_trapdoor$/,              // Includes iron, train and framed glass trapdoor.
-    /:chiseled_(?!bookshelf)/, // Don't include chiseled bookshelf.
-    /_pillar$/,
-    /:layered_/,
-
-    // ==== Wooden ====
-    /_wood$/,                  // Includes stripped wood.
-    /_hyphae$/,                // Includes stripped hyphae.
-    // Quark
-    /_post$/,                  // Includes stripped post.
-    /hollow_\w+(log|stem)/,
-    // TODO: Decorative Blocks
-    /^(?!create:).+_seat$/,    // Don't include Create seats.
-    /_palisade$/,
-    /_beam$/,
-    /_support$/,
-
-    // Create framed glass
-    "create:horizontal_framed_glass",
-    "create:vertical_framed_glass",
-
-);
+// Replaces block drops of variant blocks to ensure they always drop
+// their original block, by directly modifying their loot table.
+// For example, a stair will always drop a full block.
 
 LootJS.modifiers(event => {
     const COLORS = [ 
