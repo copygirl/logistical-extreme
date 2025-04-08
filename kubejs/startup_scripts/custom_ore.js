85e6d5f39160d4b9ef274210573f1313d8bb85cc
copygirl <copygirl@mcft.net>
2025-04-08 14:47:12 +0200

    Add quartz deposits to overworld

---
+++ kubejs/startup_scripts/custom_ore.js
@@ -0,0 +1,21 @@
+StartupEvents.registry("block", event => {
+    event.create("quartz_ore")
+        .displayName("Quartz Ore")
+        .stoneSoundType()
+        .hardness(3.0).resistance(3.0)
+        .tagBlock("mineable/pickaxe")
+        .tagBlock("minecraft:needs_iron_tool");
+    event.create("deepslate_quartz_ore")
+        .displayName("Deepslate Quartz Ore")
+        .stoneSoundType()
+        .hardness(4.5).resistance(3.0)
+        .tagBlock("mineable/pickaxe")
+        .tagBlock("minecraft:needs_iron_tool");
+
+    event.create("wanderlite_ore")
+        .displayName("Wanderlite Ore")
+        .stoneSoundType()
+        .hardness(3.0).resistance(3.0)
+        .tagBlock("mineable/pickaxe")
+        .tagBlock("minecraft:needs_iron_tool");
+});
