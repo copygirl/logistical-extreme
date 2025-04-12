9783485ed7131eb0531bfdac0b63c2cb19b9194c
copygirl <copygirl@mcft.net>
2025-04-12 19:04:06 +0200

    Add flint tools

---
+++ kubejs/startup_scripts/custom_tools.js
@@ -0,0 +1,25 @@
+ItemEvents.toolTierRegistry(event => {
+    event.add("flint", tier => {
+        tier.level = 0;
+        tier.uses  = 131;
+        tier.speed = 2.0;
+        tier.attackDamageBonus = 0.0;
+        tier.enchantmentValue  = 0;
+        tier.repairIngredient  = "minecraft:flint";
+    });
+});
+
+StartupEvents.registry("item", event => {
+    event.create("flint_axe_head"   ).displayName("Flint Axe Head");
+    event.create("flint_shovel_head").displayName("Flint Shovel Head");
+    event.create("flint_hoe_head"   ).displayName("Flint Hoe Head");
+
+    // Knife blade can be used as a simple knife as well to gather
+    // straw, so you can actually craft a regular and other tools.
+    event.create("flint_knife_blade").displayName("Flint Knife Blade")
+        .maxDamage(21).tag("farmersdelight:straw_harvesters");
+
+    event.create("flint_axe"   , "axe"   ).tier("flint").displayName("Flint Axe"   );
+    event.create("flint_shovel", "shovel").tier("flint").displayName("Flint Shovel");
+    event.create("flint_hoe"   , "hoe"   ).tier("flint").displayName("Flint Hoe"   );
+});
