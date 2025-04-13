bac81d7586c6f8acb5b2450b42555c617cbbca8e
copygirl <copygirl@mcft.net>
2025-04-14 00:41:01 +0200

    Add fluid mechanism item and recipe

--- CREDITS.md
+++ CREDITS.md
@@ -1,6 +1,6 @@
 - Forked **[Create]** to backport `SchematicRequirementRegistries` from version 6
 - Create textures were also used as basis for some of our own textures
-- **(Incomplete) Kinetic Mechanism** from [Create: Advanced Industries]
+- Various **mechanism** textures from [Create: Advanced Industries]
 
 [Create]: https://github.com/Creators-of-Create/Create
 [Create: Advanced Industries]: https://github.com/omgimanerd/create-advanced-industries
--- kubejs/startup_scripts/custom_items.js
+++ kubejs/startup_scripts/custom_items.js
@@ -2,6 +2,9 @@ StartupEvents.registry("item", event => {
     event.create("kubejs:kinetic_mechanism").displayName("Kinetic Mechanism");
     event.create("kubejs:incomplete_kinetic_mechanism", "create:sequenced_assembly").displayName("Incomplete Kinetic Mechanism");
 
+    event.create("kubejs:fluid_mechanism").displayName("Fluid Mechanism");
+    event.create("kubejs:incomplete_fluid_mechanism", "create:sequenced_assembly").displayName("Incomplete Fluid Mechanism");
+
     event.create("kubejs:saw_blade").displayName("Saw Blade");
     event.create("kubejs:drill_head").displayName("Drill Head");
 });
